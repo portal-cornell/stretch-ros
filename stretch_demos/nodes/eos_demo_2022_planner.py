c8e117c7233a1f980eae16c89e278482e4d0c965
kushal2000 <yukiluna1999@gmail.com>
2022-11-29 16:18:36 -0500

    merged launch file, added hard-code move

--- stretch_demos/nodes/eos_demo_2022_planner.py
+++ stretch_demos/nodes/eos_demo_2022_planner.py
@@ -9,12 +9,13 @@ import sys
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
 # We're going to use the quaternion type here.
-from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Quaternion, PoseWithCovarianceStamped, Twist
 
 # tf includes a handy set of transformations to move between Euler angles and quaternions (and back).
 from tf import transformations
 
 from basic_navigation import StretchNavigation
+from basic_move import Move
 
 HOME = (-1.85, 2.77, -0.85)
 
@@ -22,7 +23,15 @@ TABLE = (-0.397, 0.024, -0.85)
 
 class DemoPlanner:
     def __init__(self):
-        self.all_status = {"went_to_table": False}
+        self.all_status = {"went_to_table": False, "went_back_home": False}
+
+        self.amcl_listener = rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, self.get_position_in_map)
+
+        self.pos = None
+
+    def get_position_in_map(self, msg):
+        self.pos = msg.pose.pose.position
+        print(f"new pos: {self.pos}")
 
     def done_callback(self, curr_action):
         def callback(status, result):
@@ -40,7 +49,7 @@ if __name__ == '__main__':
     TO RUN:
 
     1. In one terminal:
-        roslaunch stretch_navigation navigation.launch map_yaml:=${HELLO_FLEET_PATH}/maps/mapping_eod_11_14.yaml
+        roslaunch stretch_demos eos_demo_2022.launch map_yaml:=${HELLO_FLEET_PATH}/maps/mapping_eod_11_14.yaml
 
     2. Then run:
         python3 /home/strech/catkin_ws/src/stretch_ros/stretch_demos/nodes/eos_demo_2022_planner.py
@@ -56,10 +65,26 @@ if __name__ == '__main__':
     # Declare a `StretchNavigation` object
     nav = StretchNavigation()
     
-    nav.go_to(HOME, planner.done_callback("went_to_table"))  # go roughly to the table
+    nav.go_to(TABLE, planner.done_callback("went_to_table"))  # go roughly to the table
 
     # Don't need this. Go to already makes planner sleep
     # while not planner.all_status["went_to_table"]:
     #     print("========================================================")
     #     print("       Waiting for Stretch to go to the table")
     #     print("========================================================")
+
+    
+
+    input("Waiting to get repositioned")
+
+    base_motion = Move()
+
+    while planner.pos.x > -1.9:
+        print("Moving stretch backward")
+        base_motion.move_x(speed=-0.3)
+
+    print("Completed")
+
+    # input("Got to target position")
+
+    # nav.go_to(HOME, planner.done_callback("went_back_home"))  # go roughly to the table
